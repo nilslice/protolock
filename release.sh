ca4e2176e9003ec50af7f71d5c1b78a72df37639
Steve Manuel <nilslice@gmail.com>
2018-05-13 12:26:53 -0600

    add release script for cross compiling binaries

---
+++ release.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set +x
+mkdir -p builds
+
+rm builds/*
+
+NOW=`date +%F.%s`
+BUILD_SRC=cmd/protolock/main.go
+NAME=protolock
+BUILD_CMD="go build -o $NAME $BUILD_SRC"
+
+LINUX_TAR="protolock-$NOW-linux.tgz"
+WIN_TAR="protolock-$NOW-windows.tgz"
+MAC_TAR="protolock-$NOW-macos.tgz"
+
+# cross compile for linux, windows, macOS
+GOOS=linux $BUILD_CMD
+tar czf $LINUX_TAR protolock README.md LICENSE
+mv $LINUX_TAR builds
+
+GOOS=windows $BUILD_CMD 
+mv protolock protolock.exe
+tar czf $WIN_TAR  protolock.exe README.md LICENSE
+mv $WIN_TAR builds
+
+GOOS=darwin $BUILD_CMD
+tar czf $MAC_TAR protolock README.md LICENSE
+mv $MAC_TAR builds
