e29e356b8165e2cb4d44a047f1b7927a6ab42425
Steve Manuel <nilslice@gmail.com>
2018-05-14 00:08:47 -0600

    fix protopath tests, use updated path sep

--- protopath_test.go
+++ protopath_test.go
@@ -9,12 +9,12 @@ import (
 func TestOSPathToProtoPath(t *testing.T) {
 	path := protopath("testdata/test.proto")
 	p := protoPath(path)
-	assert.Equal(t, "testdata:test.proto", string(p))
-	assert.Equal(t, protopath("testdata:test.proto"), p)
+	assert.Equal(t, "testdata:/:test.proto", string(p))
+	assert.Equal(t, protopath("testdata:/:test.proto"), p)
 }
 
 func TestProtoPathToOSPath(t *testing.T) {
-	path := protopath("testdata:test.proto")
+	path := protopath("testdata:/:test.proto")
 	p := osPath(path)
 	assert.Equal(t, protopath("testdata/test.proto"), p)
 	assert.Equal(t, "testdata/test.proto", string(p))
