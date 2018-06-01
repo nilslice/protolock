2db1b3fdd165edd8d055ade6fb46b570f5c4f6db
Steve Manuel <nilslice@gmail.com>
2018-06-01 09:09:28 -0600

    update tests for use of filepath pkg

--- parse_test.go
+++ parse_test.go
@@ -1,48 +1,71 @@
 package protolock
 
 import (
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+var gpfPath = filepath.Join("testdata", "getProtoFiles")
+
 func TestGetProtoFilesFiltersDirectories(t *testing.T) {
-	files, err := getProtoFiles("testdata/getProtoFiles", "")
+	files, err := getProtoFiles(gpfPath, "")
 	assert.NoError(t, err)
 
-	assert.NotContains(t, files, "testdata/getProtoFiles/directory.proto")
-	assert.Contains(t, files, "testdata/getProtoFiles/include/include.proto")
+	path := filepath.Join(gpfPath, "directory.proto")
+	assert.NotContains(t, files, path)
+
+	path = filepath.Join(gpfPath, "include", "include.proto")
+	assert.Contains(t, files, path)
 }
 
 func TestGetProtoFilesFiltersNonProto(t *testing.T) {
-	files, err := getProtoFiles("testdata/getProtoFiles", "")
+	files, err := getProtoFiles(gpfPath, "")
 	assert.NoError(t, err)
 
-	assert.NotContains(t, files, "testdata/getProtoFiles/directory.proto/test.non-proto")
-	assert.Contains(t, files, "testdata/getProtoFiles/include/include.proto")
+	path := filepath.Join(gpfPath, "directory.proto", "test.non-proto")
+	assert.NotContains(t, files, path)
+
+	path = filepath.Join(gpfPath, "include", "include.proto")
+	assert.Contains(t, files, path)
 }
 
 func TestGetProtoFilesIgnoresDirectories(t *testing.T) {
-	files, err := getProtoFiles("testdata/getProtoFiles", "exclude")
+	files, err := getProtoFiles(gpfPath, "exclude")
 	assert.NoError(t, err)
 
-	assert.NotContains(t, files, "testdata/getProtoFiles/exclude/test.proto")
-	assert.Contains(t, files, "testdata/getProtoFiles/include/include.proto")
+	path := filepath.Join(gpfPath, "exclude", "test.proto")
+	assert.NotContains(t, files, path)
+
+	path = filepath.Join(gpfPath, "include", "include.proto")
+	assert.Contains(t, files, path)
 }
 
 func TestGetProtoFilesIgnoresFiles(t *testing.T) {
-	files, err := getProtoFiles("testdata/getProtoFiles", "include/exclude.proto")
+	files, err := getProtoFiles(gpfPath, filepath.Join("include", "exclude.proto"))
 	assert.NoError(t, err)
 
-	assert.NotContains(t, files, "testdata/getProtoFiles/include/exclude.proto")
-	assert.Contains(t, files, "testdata/getProtoFiles/include/include.proto")
+	path := filepath.Join(gpfPath, "include", "exclude.proto")
+	assert.NotContains(t, files, path)
+
+	path = filepath.Join(gpfPath, "include", "include.proto")
+	assert.Contains(t, files, path)
 }
 
 func TestGetProtoFilesIgnoresMultiple(t *testing.T) {
-	files, err := getProtoFiles("testdata/getProtoFiles", "exclude,include/exclude.proto")
+	paths := []string{"exclude", filepath.Join("include", "exclude.proto")}
+	ignores := strings.Join(paths, ",")
+	files, err := getProtoFiles(gpfPath, ignores)
 	assert.NoError(t, err)
 
-	assert.NotContains(t, files, "testdata/getProtoFiles/exclude/test.proto")
-	assert.NotContains(t, files, "testdata/getProtoFiles/include/exclude.proto")
-	assert.Contains(t, files, "testdata/getProtoFiles/include/include.proto")
+	path := filepath.Join(gpfPath, "exclude", "test.proto")
+	assert.NotContains(t, files, path)
+
+	path = filepath.Join(gpfPath, "include", "exclude.proto")
+	assert.NotContains(t, files, path)
+
+	path = filepath.Join(gpfPath, "include", "include.proto")
+	assert.Contains(t, files, path)
 }
--- protopath_test.go
+++ protopath_test.go
@@ -1,13 +1,16 @@
 package protolock
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+var osp = filepath.Join("testdata", "test.proto")
+
 func TestOSPathToProtoPath(t *testing.T) {
-	path := protopath("testdata/test.proto")
+	path := protopath(osp)
 	p := protoPath(path)
 	assert.Equal(t, "testdata:/:test.proto", string(p))
 	assert.Equal(t, protopath("testdata:/:test.proto"), p)
@@ -16,6 +19,6 @@ func TestOSPathToProtoPath(t *testing.T) {
 func TestProtoPathToOSPath(t *testing.T) {
 	path := protopath("testdata:/:test.proto")
 	p := osPath(path)
-	assert.Equal(t, protopath("testdata/test.proto"), p)
-	assert.Equal(t, "testdata/test.proto", string(p))
+	assert.Equal(t, protopath(osp), p)
+	assert.Equal(t, osp, string(p))
 }
