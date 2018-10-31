122edc213497855d6cea400603c3dba01007495d
Steve Manuel <nilslice@gmail.com>
2018-10-31 02:14:33 -0600

    fix docs in exported code

--- protopath.go
+++ protopath.go
@@ -10,16 +10,17 @@ const (
 	protosep = ":/:"
 )
 
+// Protopath is a type to assist in OS filepath transformations
 type Protopath string
 
-// convert a path in the Protopath format to the OS path format
+// OSPath converts a path in the Protopath format to the OS path format
 func OSPath(ProtoPath Protopath) Protopath {
 	return Protopath(
 		strings.Replace(string(ProtoPath), protosep, filesep, -1),
 	)
 }
 
-// convert a path in the OS path format to Protopath format
+// ProtoPath converts a path in the OS path format to Protopath format
 func ProtoPath(OSPath Protopath) Protopath {
 	return Protopath(
 		strings.Replace(string(OSPath), filesep, protosep, -1),
