9fe0f11ecc28d872d17dd03e50add320fb31892d
Steve Manuel <nilslice@gmail.com>
2018-10-31 14:57:05 -0600

    fix gitignore and revert circle config changes

---
+++ cmd/protolock/plugins.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os/exec"
+	"strings"
+	"sync"
+
+	"github.com/nilslice/protolock"
+	"github.com/nilslice/protolock/extend"
+)
+
+func runPlugins(pluginList string, report *protolock.Report) (*protolock.Report, error) {
+	inputData := &bytes.Buffer{}
+
+	err := json.NewEncoder(inputData).Encode(&extend.Data{
+		Current:        report.Current,
+		Updated:        report.Updated,
+		PluginWarnings: []protolock.Warning{},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// collect plugin warnings and errors as they are returned from plugins
+	pluginWarningsChan := make(chan []protolock.Warning)
+	pluginsDone := make(chan struct{})
+	pluginErrsChan := make(chan error)
+	var allPluginErrors []error
+	go func() {
+		for {
+			select {
+			case <-pluginsDone:
+				break
+
+			case err := <-pluginErrsChan:
+				if err != nil {
+					allPluginErrors = append(allPluginErrors, err)
+				}
+
+			case warnings := <-pluginWarningsChan:
+				for _, warning := range warnings {
+					report.Warnings = append(report.Warnings, warning)
+				}
+			}
+		}
+	}()
+
+	defer func() {
+		pluginsDone <- struct{}{}
+	}()
+
+	wg := &sync.WaitGroup{}
+	plugins := strings.Split(pluginList, ",")
+	for _, name := range plugins {
+		wg.Add(1)
+		// run all provided plugins in parallel, each recieving the same current
+		// and updated Protolock structs from the `protolock status` call
+		go func() {
+			defer wg.Done()
+			name = strings.TrimSpace(name)
+			path, err := exec.LookPath(name)
+			if err != nil {
+				if path == "" {
+					path = name
+				}
+				fmt.Println("[protolock] plugin exec error:", err)
+				return
+			}
+
+			plugin := &exec.Cmd{
+				Path:  path,
+				Stdin: inputData,
+			}
+
+			// execute the plugin and capture the output
+			output, err := plugin.Output()
+			if err != nil {
+				pluginErrsChan <- wrapPluginErr(name, path, err)
+				return
+			}
+
+			pluginData := &extend.Data{}
+			err = json.Unmarshal(output, pluginData)
+			if err != nil {
+				fmt.Println("[protolock] plugin data decode error:", err)
+				return
+			}
+
+			if pluginData.PluginWarnings != nil {
+				pluginWarningsChan <- pluginData.PluginWarnings
+			}
+
+			if pluginData.PluginErrorMessage != "" {
+				pluginErrsChan <- wrapPluginErr(
+					name, path, errors.New(pluginData.PluginErrorMessage),
+				)
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	if allPluginErrors != nil {
+		var errorMsgs []string
+		for _, pluginError := range allPluginErrors {
+			errorMsgs = append(errorMsgs, pluginError.Error())
+		}
+
+		return nil, fmt.Errorf(
+			`[protolock:plugin] accumulated plugin errors:
+%s`,
+			strings.Join(errorMsgs, "\n"),
+		)
+	}
+
+	return report, nil
+}
+
+func wrapPluginErr(name, path string, err error) error {
+	return fmt.Errorf("%s: %v (%s)", name, err, path)
+}
